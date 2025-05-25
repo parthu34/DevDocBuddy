31664ea16d430da9c15b525cf384a76e5e0af8c4
parthu34 <parth.darji34@gmail.com>
2025-05-25 19:34:32 -0400

    feat(frontend): add DocInput and DocResults components for query input and summary display

---
+++ frontend/src/components/DocInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <input v-model="store.query" placeholder="Paste doc URL or text here" />
+    <button @click="store.fetchSummary" :disabled="store.loading">Summarize</button>
+    <p v-if="store.error" class="error">{{ store.error }}</p>
+  </div>
+</template>
+
+<script setup>
+import { useDocStore } from '../stores/docStore'
+const store = useDocStore()
+</script>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
