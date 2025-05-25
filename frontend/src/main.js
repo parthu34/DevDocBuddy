59be3938f8768d7d7edbe679e86a428ade0a9d4c
parthu34 <parth.darji34@gmail.com>
2025-05-25 19:36:43 -0400

    chore(frontend): integrate Element Plus UI framework

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
 
 const app = createApp(App)
+app.use(ElementPlus)
 app.use(router)
 app.use(createPinia())
 app.mount('#app')
